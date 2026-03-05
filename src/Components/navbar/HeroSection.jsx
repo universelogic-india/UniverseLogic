6b56bdf1f27e7f10c239fd78a723ccd51a7c35d5
universelogic-india <ecommerce02777@gmail.com>
2026-03-05 06:23:29 +0530

    Update HeroSection.jsx

--- src/Components/navbar/HeroSection.jsx
+++ src/Components/navbar/HeroSection.jsx
@@ -28,7 +28,7 @@ const HeroSection = () => {
       
 
         <div className="bg-img">
-          <img src="/src/assets/hero-img.png" alt="Hero-Background" className='responsive-img'/>
+          <img src="/hero-img.png" alt="Hero-Background" className='responsive-img'/>
         </div>    
     </div>
     
