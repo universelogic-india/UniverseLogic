3bba91007cb800676733efb6bff1c97f0e44c090
universelogic-india <ecommerce02777@gmail.com>
2026-03-05 06:41:28 +0530

    Update HeroSection.jsx

--- src/Components/navbar/HeroSection.jsx
+++ src/Components/navbar/HeroSection.jsx
@@ -1,5 +1,5 @@
 import './heroSection.css';
-import hero-img from '../src/assets/hero-img.png';
+import hero_img from '../src/assets/hero-img.png';
 import Typewriter from 'typewriter-effect';
 
 const HeroSection = () => {
@@ -29,7 +29,7 @@ const HeroSection = () => {
       
 
         <div className="bg-img">
-          <img src="/src/assets/hero-img.png" alt="Hero-Background" className='responsive-img'/>
+          <img src="/src/assets/hero_img.png" alt="Hero-Background" className='responsive-img'/>
         </div>    
     </div>
     
